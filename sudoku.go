89c59f5dd7c68806e26e35eeb3d5946174edca97
Charles Dunbar <ccdunbar@gmail.com>
2020-08-15 12:57:39 -0500

    Refactored with cell type

--- sudoku.go
+++ sudoku.go
@@ -7,7 +7,11 @@ import (
 	"strings"
 )
 
-type board [][]int
+type board [][]cell
+
+type cell struct {
+	x, y, value int
+}
 
 func (b board) String() string {
 	r := ""
@@ -18,7 +22,7 @@ func (b board) String() string {
 			} else {
 				r += fmt.Sprintf("%d ", b[i][j])
 			}*/
-			r += fmt.Sprintf("%d ", b[i][j])
+			r += fmt.Sprintf("%d ", b[i][j].value)
 		}
 		r += "\n"
 	}
@@ -28,13 +32,15 @@ func (b board) String() string {
 func genBoard(p string) board {
 	b := make(board, 9)
 	for x := 0; x < 9; x++ {
-		b[x] = make([]int, 9)
+		b[x] = make([]cell, 9)
 	}
 	spl := strings.Split(p, "")
 
 	count := 0
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
+			b[i][j].x = i
+			b[i][j].y = j
 			if spl[count] == "." {
 				count++
 				continue
@@ -43,7 +49,7 @@ func genBoard(p string) board {
 				if err != nil {
 					log.Fatal(err)
 				}
-				b[i][j] = val
+				b[i][j].value = val
 				count++
 			}
 		}
@@ -51,21 +57,37 @@ func genBoard(p string) board {
 	return b
 }
 
-func chkRow(x int, b *board) bool {
-	row := removeZeroes((*b)[x])
+func chkLine(x []int) bool {
+	row := removeZeroes(x)
 	uniq := unique(row)
 	return len(uniq) == len(row)
 }
 
-func chkCol(y int, b *board) bool {
-	return true
-}
+/*func chkBox(x, y int, b *board) bool {
+	// Generate an []int to pass to chkLine based on the 3x3 square x and y are in
+	val := [][]int{[]int{x, y}}
+	box := []int{}
 
-func chkBox(b *board) bool {
+	box1 := [][]int{[]int{0, 0}, []int{0, 1}, []int{0, 2}, []int{1, 0}, []int{1, 1}, []int{1, 2}, []int{2, 0}, []int{2, 1}, []int{2, 2}}
+
+	//[0][0], [0][1], [0][2], [1][0], [1][1], [1][2], [2][0], [2][1], [2][2] = box 1
+
+
 	return true
-}
+} /*
+
+// From https://stackoverflow.com/a/10485970
+/*func contains(s [][]int, e []int) bool {
+	for i, j := range s {
+		if s[i] == e {
+			return true
+		}
+	}
+	return false
+}*/
 
 // From https://www.golangprograms.com/remove-duplicate-values-from-slice.html
+// Takes a slice and returns only unique values
 func unique(intSlice []int) []int {
 	keys := make(map[int]bool)
 	list := []int{}
@@ -90,22 +112,43 @@ func removeZeroes(intSlice []int) []int {
 
 func validateBoard(x, y int, b *board) bool {
 	// TODO - These can be concurrent
-	row := chkRow(x, b)
-	col := chkCol(y, b)
-	box := chkBox(b)
-	return row && col && box
+	genRow := lineFilter(b, func(val cell) bool {
+		return val.x == x
+	})
+	row := chkLine(genRow)
+
+	genCol := lineFilter(b, func(val cell) bool {
+		return val.y == y
+	})
+	col := chkLine(genCol)
+
+	//box := chkBox(x, y, b)
+	return row && col
 }
 
-func solve(b *board) {
+/*func solve(b *board) {
 	var curRow, curCol int = 0, 0
 	for {
 		(*b)[curRow][curCol]++
 		validateBoard(curRow, curCol, b)
 	}
 
+}*/
+
+func lineFilter(b *board, f func(cell) bool) []int {
+	line := []int{}
+	for i := range *b {
+		for j := range (*b)[i] {
+			if f((*b)[i][j]) {
+				line = append(line, (*b)[i][j].value)
+			}
+		}
+	}
+	return line
 }
 
 func main() {
 	b := genBoard("......9.....5....85..83....35..82.....6...2....937..4.76........3.4.5.76..2......")
 	fmt.Println(b)
+	fmt.Println(validateBoard(0, 0, &b))
 }
--- sudoku_test.go
+++ sudoku_test.go
@@ -2,19 +2,19 @@ package main
 
 import "testing"
 
-func TestChkRow(t *testing.T) {
+func TestChkLine(t *testing.T) {
 	// TODO - set up a completed board to test for success, edit to test failures
-	validRow := board{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9}}
-	invalidRow := board{[]int{9, 2, 3, 4, 3, 6, 7, 8, 9}}
-	zeroRow := board{[]int{0, 2, 3, 0, 0, 6, 7, 0, 9}}
+	validRow := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	invalidRow := []int{9, 2, 3, 4, 3, 6, 7, 8, 9}
+	zeroRow := []int{0, 2, 3, 0, 0, 6, 7, 0, 9}
 
-	if !chkRow(0, &validRow) {
+	if !chkLine(validRow) {
 		t.Errorf("Row %d didn't pass as a valid row when it should", validRow[0])
 	}
-	if chkRow(0, &invalidRow) {
+	if chkLine(invalidRow) {
 		t.Errorf("Row %d passed as a valid row when it shouldn't", invalidRow[0])
 	}
-	if !chkRow(0, &zeroRow) {
+	if !chkLine(zeroRow) {
 		t.Errorf("Row %d didn't pass as a valid row when it should", zeroRow[0])
 	}
 }
